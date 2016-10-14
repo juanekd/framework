578b8a56279bd93b4e8bc6f3d0041778a55668e7
juanekd <juan_ek_dzul@hotmail.com>
2016-10-13 22:09:36 -0500

    modificacion del header

--- views/layouts/default/header.php
+++ views/layouts/default/header.php
@@ -3,5 +3,25 @@
 <head>
 <meta charset="UTF-8">
 	<title>Framework Básico</title>
+	<style>
+		ul{
+			padding: 0;
+		}
+		ul li{
+			display: inline;
+		}
+		nav a{
+			text-decoration: none;
+		}
+	</style>
 </head>
 <body>
+ <nav>
+ 	<ul>
+ 		<li><a href="/">inicio</a></li>
+ 		<li><a href="#">servicio</a></li>
+ 		<li><a href="#">Productos</a></li>
+ 		<li><a href="#">notificaciones</a></li>
+ 		<li><a href="#">Contactanos</a></li>
+ 	</ul>
+ </nav>
